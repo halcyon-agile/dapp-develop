a3fdc7ad8e236f533be0002dbfe1c4577d1b7036
John Steve Palero <logicallydimp@gmail.com>
2023-10-19 15:58:33 +0800

    chore: change portalUrl

--- src/lib/portalUrl.ts
+++ src/lib/portalUrl.ts
@@ -1,6 +1,6 @@
 const portalUrl =
   import.meta.env.VITE_MODE === "DEV"
     ? import.meta.env.VITE_DEV_URL
-    : "https://lively-geyser-q53l27l9w5bc.vapor-farm-e1.com";
+    : import.meta.env.VITE_PROD_URL;
 
 export default portalUrl;
